a2c47258cb4c41ed13f26b70482c25c147b92819
Razumov Alexandr <arazumov@ddestiny.ru>
2015-03-24 16:42:38 +0300

    updated tests

--- runtests.py
+++ runtests.py
@@ -7,9 +7,13 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
 from django.conf import settings
 from django.test.utils import get_runner
 
-if __name__ == "__main__":
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
-    TestRunner = get_runner(settings)
-    test_runner = TestRunner()
+
+def run():
+    r = get_runner(settings)
+    test_runner = r()
     failures = test_runner.run_tests(["tests"])
     sys.exit(bool(failures))
+
+
+if __name__ == "__main__":
+    run()
