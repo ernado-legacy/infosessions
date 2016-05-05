5cf3278d3790f601ef64fcdd5dc3fe3b3a96e09a
Aleksandr Razumov <ar@cydev.ru>
2016-05-05 21:11:57 +0300

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-infosessions',
-    version='0.5',
+    version='0.6',
     packages=['infosessions'],
     include_package_data=True,
     test_suite='runtests.run',
