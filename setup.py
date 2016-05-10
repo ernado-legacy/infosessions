c3d24c5958fbab29d502afce9bc607655a168e51
Aleksandr Razumov <ar@cydev.ru>
2016-05-10 17:21:07 +0300

    add migrations and bump version

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -9,24 +9,23 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-infosessions',
-    version='0.6',
-    packages=['infosessions'],
+    version='1.0',
+    packages=find_packages(),
     include_package_data=True,
     test_suite='runtests.run',
-    license='BSD License',  # example license
+    license='BSD License',
     description='A simple Django app to capture session information',
     long_description=README,
     author='Aleksandr Razumov',
-    author_email='ernado@ya.ru',
+    author_email='ar@cydev.ru',
     install_requires=['django>=1.8', 'django-redis>=3.8.3', 'hiredis>=0.1.4'],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License', # example license
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        # Replace these appropriately if you are stuck on Python 2.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP',
